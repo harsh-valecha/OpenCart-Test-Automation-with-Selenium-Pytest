85751913bc057bca2335005c3508c800fb847d00
Harsh <harsh.valecha@yahoo.com>
2024-08-18 16:40:10 +0530

    fixed the bug for inserting data

--- utils/database_reader.py
+++ utils/database_reader.py
@@ -38,10 +38,10 @@ def select_data_sql(query):
 
 
 # for unit testing
-categories = [['All Categories'], ['Desktops'], ['PC'], ['Mac'], ['Laptops & Notebooks'], ['Macs'], ['Windows'], ['Components'], ['Mice and Trackballs'], ['Monitors'], ['test 1'], ['test 2'], ['Printers'], ['Scanners'], ['Web Cameras'], ['Tablets'], ['Software'], ['Phones & PDAs'], ['Cameras'], ['MP3 Players'], ['test 11'], ['test 12'], ['test 15'], ['test 16'], ['test 17'], ['test 18'], ['test 19'], ['test 20'], ['test 25'], ['test 21'], ['test 22'], ['test 23'], ['test 24'], ['test 4'], ['test 5'], ['test 6'], ['test 7'], ['test 8'], ['test 9']]
-query = '''
-insert into product_categories(category) values(%s);
-'''
-insert_data_sql(query,data=categories)
+# categories = [['All Categories'], ['Desktops'], ['PC'], ['Mac'], ['Laptops & Notebooks'], ['Macs'], ['Windows'], ['Components'], ['Mice and Trackballs'], ['Monitors'], ['test 1'], ['test 2'], ['Printers'], ['Scanners'], ['Web Cameras'], ['Tablets'], ['Software'], ['Phones & PDAs'], ['Cameras'], ['MP3 Players'], ['test 11'], ['test 12'], ['test 15'], ['test 16'], ['test 17'], ['test 18'], ['test 19'], ['test 20'], ['test 25'], ['test 21'], ['test 22'], ['test 23'], ['test 24'], ['test 4'], ['test 5'], ['test 6'], ['test 7'], ['test 8'], ['test 9']]
+# query = '''
+# insert into product_categories(category) values(%s);
+# '''
+# insert_data_sql(query,data=categories)
 
 
