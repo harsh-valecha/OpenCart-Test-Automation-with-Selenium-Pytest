23895dbc5d7355b1daa2e35c70bf7f1f7496b0c1
Harsh <harsh.valecha@yahoo.com>
2024-08-22 12:49:25 +0530

    updated code to wait explicitly till it gets the url

--- tests/regression/test_add_to_cart.py
+++ tests/regression/test_add_to_cart.py
@@ -4,15 +4,21 @@ from selenium.webdriver.common.by import By
 from opencart_automation.pages.search_page import SearchPage
 from opencart_automation.tests.conftest import login
 import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 def test_add_to_cart_from_search(driver:WebDriver):
     driver = login(driver)
     page = SearchPage(driver)
     page.type_keywords_and_enter('apple')
     page.click_add_to_cart()
-    # driver.execute_script("arguments[0].style.display='block';", btn)
-    # btn.click()
-    # driver.execute_script("arguments[0].click();",btn)
-    time.sleep(2)
-    assert driver.current_url =='https://awesomeqa.com/ui/index.php?route=product/product&product_id=42'
+    #time.sleep(2)
+    try:
+        WebDriverWait(driver,2).until(
+            EC.url_matches('https://awesomeqa.com/ui/index.php?route=product/product&product_id=42')
+        )
+    except Exception as e:
+        print(f'This was the exception:{e}')
+    finally:
+        assert driver.current_url =='https://awesomeqa.com/ui/index.php?route=product/product&product_id=42'
 
